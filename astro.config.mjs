e30131dcf2305f7b8ae80b74bb2086ea871a4c1f
Brendan Tierney <tiernebre@gmail.com>
2024-01-05 21:06:27 +0000

    custom domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tiernebre.github.io/",
+  site: "https://tiernebre.com/",
   integrations: [tailwind()]
 });
